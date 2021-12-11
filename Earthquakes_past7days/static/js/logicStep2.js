f4952feeb0e8e4c87006e77403816c188ce1f035
Mike Thalken <mthalken53@gmail.com>
2021-12-11 11:05:16 -0800

    Changed points on map to circle based on magnitude.

---
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -0,0 +1,115 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let street = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_key
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 10,
+    accessToken: API_key
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: street,
+    Satellite: satelliteStreets
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [street]
+})
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+    return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: "#ffae42",
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+    };
+}
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+    return magnitude * 4;
+}
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo
+    }).addTo(map);
+});
+
+
+// // Accessing the Toronto neighborhoods GeoJSON URL.
+// let torontoHoods = "https://raw.githubusercontent.com/mthalken/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//     console.log(data);
+//     // Creating a GeoJSON layer with the retrieved data.
+//     L.geoJSON(data, {
+//         onEachFeature: function(feature, layer) {
+//             console.log(layer);
+//             layer.bindPopup("<h2> Airport code: " + feature.properties.faa + "</h2> <hr> <h3> Airport name: " + feature.properties.name + "</h3");
+//         }
+//     }).addTo(map);
+// });
+
+// // Accessing the Toronto airline routes GeoJSON URL.
+// let torontoData = "https://raw.githubusercontent.com/mthalken/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoData).then(function(data) {
+//     L.geoJSON(data, {
+//         style: myStyle,
+//         onEachFeature: function(feature, layer) {
+//             layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: " + feature.properties.dst + "</h3>");
+//         }
+//     }).addTo(map);
+// });
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// options for maps style:
+// mapbox/streets-v11
+// mapbox/outdoors-v11
+// mapbox/light-v10
+// mapbox/dark-v10
+// mapbox/satellite-v9
+// mapbox/satellite-streets-v11
